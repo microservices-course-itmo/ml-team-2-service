1e0da04592c5cd20696940f826ebb7f36ac52009
Aleksandr Semiletov <alexander.semiletov@corp.mail.ru>
2020-12-02 00:07:16 +0300

    Change env vars

--- src/server/server/settings.py
+++ src/server/server/settings.py
@@ -75,19 +75,13 @@ WSGI_APPLICATION = "server.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE"),
-        "NAME": os.environ.get("SQL_DATABASE"),
-        "USER": os.environ.get("SQL_USER"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD"),
-        "HOST": os.environ.get("SQL_HOST"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("S_POSTGRES_DB"),
+        "USER": os.environ.get("S_POSTGRES_USER"),
+        "PASSWORD": os.environ.get("S_POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("S_POSTGRES_HOST"),
         "PORT": os.environ.get("SQL_PORT"),
     }
 }
