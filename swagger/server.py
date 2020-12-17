86e94dd9adadf8dcacbefd7c2c192e33871a3721
Aleksandr Semiletov <alexander.semiletov@corp.mail.ru>
2020-12-17 19:27:00 +0300

    Add swagger UI

---
+++ swagger/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
+
+app = Flask(__name__, static_url_path="/static")
+
+SWAGGER_URL = "/swagger-ui.html"
+API_URL = "/swagger-ui.html/api.yaml"
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+)
+
+
+@app.route("/swagger-ui.html/api.yaml")
+def root():
+    return app.send_static_file("api.yaml")
+
+
+app.register_blueprint(swaggerui_blueprint)
+
+app.run(debug=True, port=5000)
