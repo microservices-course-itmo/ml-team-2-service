ed6ac2b7c4e88793426359a345934d8d474e207f
Aleksandr Semiletov <alexander.semiletov@corp.mail.ru>
2020-12-17 19:52:41 +0300

    Fix swagger path

--- swagger/server.py
+++ swagger/server.py
@@ -3,8 +3,8 @@ from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__, static_url_path="/static")
 
-SWAGGER_URL = "/swagger-ui.html"
-API_URL = "/swagger-ui.html/api.yaml"
+SWAGGER_URL = "/ml-team-2-service/swagger-ui.html"
+API_URL = "/ml-team-2-service/swagger-ui.html/api.yaml"
 
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
@@ -12,7 +12,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 )
 
 
-@app.route("/swagger-ui.html/api.yaml")
+@app.route("/ml-team-2-service/swagger-ui.html/api.yaml")
 def root():
     return app.send_static_file("api.yaml")
 
