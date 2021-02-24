24ede2dac344212a12b5407cc581b4515cad51db
Anna Doshchenko <annadoshenko3@gmail.com>
2021-02-24 17:23:27 +0300

    Another attempt to fix csrf

--- src/server/server/settings.py
+++ src/server/server/settings.py
@@ -86,10 +86,16 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+MIDDLEWARE_CLASSES = (
+    "ml-team-2-service.disable.DisableCSRF",
+)
+
 
 ROOT_URLCONF = "server.urls"
 
