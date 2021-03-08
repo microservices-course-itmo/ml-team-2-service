2dfbeb5e1f5eb4c87f64bc706954cb01cc723fd1
Anna Doshchenko <annadoshenko3@gmail.com>
2021-03-08 21:57:36 +0300

    Some changes in consumers

--- catalog_consumer.py
+++ catalog_consumer.py
@@ -3,9 +3,8 @@ from json import loads
 import os
 
 TOPIC = 'eventTopic'
-BOOTSTRAP_SERVER = [os.environ.get('S_KAFKA_BOOTSTRAP_HOST')]
-#BOOTSTRAP_SERVER = ['kafka:9092']
-print(BOOTSTRAP_SERVER)
+#BOOTSTRAP_SERVER = [os.environ.get('S_KAFKA_BOOTSTRAP_HOST')]
+BOOTSTRAP_SERVER = ['kafka:9092']
 AUTO_OFFSET_RESET = 'earliest' # after breaking down consumer restarts reading at the latest commit offset
 ENABLE_AUTO_COMMIT = True # makes sure the consumer commits its read offset every interval
 AUTO_COMMIT_INTERVAL = 1 # 1 second
@@ -14,11 +13,13 @@ GROUP_ID = 'wine.catalog-service' # consumer needs to be a part of a consumer gr
 consumer = KafkaConsumer(
     TOPIC,
     bootstrap_servers=BOOTSTRAP_SERVER,
-    #auto_offset_reset=AUTO_OFFSET_RESET,
+    auto_offset_reset=AUTO_OFFSET_RESET,
     #enable_auto_commit=ENABLE_AUTO_COMMIT,
     group_id=GROUP_ID,
 )
-
-for message in consumer:
-    message = message.value
-    print(message)
+def get_message(consumer):
+    messages = []
+    for message in consumer:
+        UpdatePriceEvent = message.value
+        messages.append(UpdatePriceEvent)
+        print(UpdatePriceEvent)
