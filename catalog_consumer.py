80d750c82569c9e47d3ec11f26e3f359a899992f
Anna Doshchenko <annadoshenko3@gmail.com>
2021-03-09 18:53:00 +0300

    Some additions in consumer. Sending messages to kafka locally and reading it

--- catalog_consumer.py
+++ catalog_consumer.py
@@ -1,7 +1,9 @@
-from kafka import KafkaConsumer
+import segmentation_pb2 as msg
+from kafka import KafkaConsumer, KafkaProducer
 from json import loads
 import os
 
+<<<<<<< Updated upstream
 TOPIC = "eventTopic"
 # BOOTSTRAP_SERVER = [os.environ.get('S_KAFKA_BOOTSTRAP_HOST')]
 BOOTSTRAP_SERVER = ["kafka:9092"]
@@ -11,7 +13,42 @@ ENABLE_AUTO_COMMIT = (
 )
 AUTO_COMMIT_INTERVAL = 1  # 1 second
 GROUP_ID = "wine.catalog-service"  # consumer needs to be a part of a consumer group
+=======
+TOPIC = 'eventTopic'
+BOOTSTRAP_SERVER = ['localhost:9092']
+AUTO_OFFSET_RESET = 'earliest' # after breaking down consumer restarts reading at the latest commit offset
+ENABLE_AUTO_COMMIT = True # makes sure the consumer commits its read offset every interval
+AUTO_COMMIT_INTERVAL = 1 # 1 second
+GROUP_ID = 'wine.catalog-service' # consumer needs to be a part of a consumer group
+>>>>>>> Stashed changes
 
+def connect_kafka_producer():
+    _producer = None
+    try:
+        _producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+    except Exception:
+        pass
+    finally:
+        return _producer
+
+def publish_message(producer_instance, topic_name, data):
+    try:
+        producer_instance.send(topic_name, data.SerializeToString())#key=key_bytes, value=value_bytes)
+        producer_instance.flush()
+        print('Message successfully sent...\n')
+    except Exception as e:
+        print(str(e))
+
+def get_message(consumer):
+    messages = []
+    for message in consumer:
+        UpdatePriceEvent = message.value
+        messages.append(UpdatePriceEvent)
+        print(UpdatePriceEvent)
+    return(messages)
+
+producer = connect_kafka_producer()
+print("Poducer innitialized")
 consumer = KafkaConsumer(
     TOPIC,
     bootstrap_servers=BOOTSTRAP_SERVER,
@@ -20,10 +57,15 @@ consumer = KafkaConsumer(
     group_id=GROUP_ID,
 )
 
+segm = msg.Segmentation()
+segm2 = msg.Segmentation()
 
-def get_message(consumer):
-    messages = []
-    for message in consumer:
-        UpdatePriceEvent = message.value
-        messages.append(UpdatePriceEvent)
-        print(UpdatePriceEvent)
+segm.segm_link = "http://we`re_ml-2"
+segm.mask_link = "http://ml-2_team"
+segm.id = 1
+
+print(segm)
+# this is just to check locally if it`s working or not
+publish_message(producer, TOPIC, segm)#'msg', str(serialized))
+links = get_message(consumer)
+print(links)
--- user_consumer.py
+++ user_consumer.py
@@ -2,6 +2,7 @@ from kafka import KafkaConsumer
 from json import loads
 import os
 
+<<<<<<< Updated upstream
 TOPIC = "eventTopic"  # needed to be changed
 # BOOTSTRAP_SERVER = [os.environ.get('S_KAFKA_BOOTSTRAP_HOST')]
 BOOTSTRAP_SERVER = ["localhost:9092"]
@@ -11,9 +12,26 @@ ENABLE_AUTO_COMMIT = (
 )
 AUTO_COMMIT_INTERVAL = 1  # 1 second
 GROUP_ID = "wine.user-service"  # consumer needs to be a part of a consumer group
+=======
+TOPIC_USERS = 'service-user-updated' # needed to be changed
+TOPIC_FAVOURITE = 'user-service-favorites-updated'
+BOOTSTRAP_SERVER = ['kafka:9092']
+AUTO_OFFSET_RESET = 'earliest' # after breaking down consumer restarts reading at the latest commit offset
+ENABLE_AUTO_COMMIT = True # makes sure the consumer commits its read offset every interval
+AUTO_COMMIT_INTERVAL = 1 # 1 second
+GROUP_ID = 'wine.user-service' # consumer needs to be a part of a consumer group
+>>>>>>> Stashed changes
 
-consumer = KafkaConsumer(
-    TOPIC,
+consumer_users = KafkaConsumer(
+    TOPIC_USERS,
+    bootstrap_servers=BOOTSTRAP_SERVER,
+    auto_offset_reset=AUTO_OFFSET_RESET,
+    #enable_auto_commit=ENABLE_AUTO_COMMIT,
+    group_id=GROUP_ID,
+)
+
+consumer_favourites = KafkaConsumer(
+    TOPIC_FAVOURITE,
     bootstrap_servers=BOOTSTRAP_SERVER,
     auto_offset_reset=AUTO_OFFSET_RESET,
     # enable_auto_commit=ENABLE_AUTO_COMMIT,
@@ -27,3 +45,7 @@ def get_message(consumer):
         message = message.value
         messages.append(message)
         print(message)
+<<<<<<< Updated upstream
+=======
+    return messages
+>>>>>>> Stashed changes
