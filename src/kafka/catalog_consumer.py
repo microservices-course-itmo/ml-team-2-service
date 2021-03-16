6f221436229ce1f2a4921b42bbf0b426aa34c19f
Anna Doshchenko <annadoshenko3@gmail.com>
2021-03-16 17:07:04 +0300

    Some refactoring is made

--- src/kafka/catalog_consumer.py
+++ src/kafka/catalog_consumer.py
@@ -1,18 +1,15 @@
 import segmentation_pb2 as msg
 from kafka import KafkaConsumer, KafkaProducer
-from json import loads
+import os
 
 """
-This file contains code that must be changed when catalog service will do there job and create necessary topics for us
+This file contains just test code that must be changed when catalog service will do there job and create necessary topics for us.
+It sends some message to kafka and then reads it and append results to log/
 """
 
 TOPIC = "eventTopic"
-BOOTSTRAP_SERVER = ["localhost:29092"]
+BOOTSTRAP_SERVER = [os.environ.get("S_CATALOG_KAFKA_HOST")]
 AUTO_OFFSET_RESET = "earliest"  # after breaking down consumer restarts reading at the latest commit offset
-ENABLE_AUTO_COMMIT = (
-    True  # makes sure the consumer commits its read offset every interval
-)
-AUTO_COMMIT_INTERVAL = 1  # 1 second
 GROUP_ID = "wine.catalog-service"  # consumer needs to be a part of a consumer group
 
 log = []
@@ -32,13 +29,8 @@ def connect_kafka_producer():
 
 def publish_message(producer_instance, topic_name, data):
     try:
-        # key_bytes = bytes(str(key), encoding='utf-8')
-        # value_bytes = bytes(str(value), encoding='utf-8')
-        # print(data)
         print(data)
-        producer_instance.send(
-            topic, data.SerializeToString()
-        )  # key=key_bytes, value=value_bytes)
+        producer_instance.send(topic, data.SerializeToString())
         producer_instance.flush()
         log.append("Message successfully sent")
     except Exception as e:
@@ -50,9 +42,9 @@ def get_message(consumer):
     for message in consumer:
         value = message.value
         link = msg.Segmentation()
-        # link.ParseFromString(value)
+        link.ParseFromString(value)
         log.append("Message successfully received")
-        # log.append(str(link))
+        log.append(str(link))
         links.append(link)
         return links
 
@@ -63,7 +55,6 @@ consumer = KafkaConsumer(
     TOPIC,
     bootstrap_servers=BOOTSTRAP_SERVER,
     auto_offset_reset=AUTO_OFFSET_RESET,
-    # enable_auto_commit=ENABLE_AUTO_COMMIT,
     group_id=GROUP_ID,
 )
 
--- src/kafka/user_consumer.py
+++ src/kafka/user_consumer.py
@@ -1,25 +1,18 @@
 from kafka import KafkaConsumer
-from json import loads
 import os
-import json
 import favorites_updated_event_pb2 as favorites
 import user_updated_event_pb2 as user
 
 TOPIC_USERS = "user-service-user-updated"  # needed to be changed
 TOPIC_FAVOURITE = "user-service-favorites-updated"
-BOOTSTRAP_SERVER = ["localhost:29092"]
+BOOTSTRAP_SERVER = [os.environ.get("S_CATALOG_KAFKA_HOST")]  # ["localhost:29092"]
 AUTO_OFFSET_RESET = "earliest"  # after breaking down consumer restarts reading at the latest commit offset
-ENABLE_AUTO_COMMIT = (
-    True  # makes sure the consumer commits its read offset every interval
-)
-AUTO_COMMIT_INTERVAL = 1  # 1 second
 GROUP_ID = "wine.user-service"  # consumer needs to be a part of a consumer group
 
 consumer_users = KafkaConsumer(
     TOPIC_USERS,
     bootstrap_servers=BOOTSTRAP_SERVER,
     auto_offset_reset=AUTO_OFFSET_RESET,
-    # enable_auto_commit=ENABLE_AUTO_COMMIT,
     group_id=GROUP_ID,
 )
 
@@ -27,7 +20,6 @@ consumer_favourites = KafkaConsumer(
     TOPIC_FAVOURITE,
     bootstrap_servers=BOOTSTRAP_SERVER,
     auto_offset_reset=AUTO_OFFSET_RESET,
-    # enable_auto_commit=ENABLE_AUTO_COMMIT,
     group_id=GROUP_ID,
 )
 
@@ -57,5 +49,8 @@ def get_message_user(consumer):
 
 
 if __name__ == "__main__":
+    """
+    Suppose this 2 functions should work in parallel, will make it later
+    """
     # get_message_favourites(consumer_favourites)
     get_message_user(consumer_users)
