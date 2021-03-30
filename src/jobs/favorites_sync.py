87612a733744307c094b32a7a6793a9567c491ce
sasha.bar99@gmail.com <Sasha.bar99>
2021-03-30 17:43:06 +0300

    Added logging to sync scripts

--- src/jobs/favorites_sync.py
+++ src/jobs/favorites_sync.py
@@ -3,19 +3,21 @@ import json
 import requests
 from tqdm import tqdm
 import logging
+import logstash
 
 OUR_ADDRESS = os.environ["S_OUR_ADDRESS"]
 USER_ADDRESS = os.environ["S_USER_ADDRESS"]
+logstash_host = os.environ.get("S_LOGSTASH_HOST")
 
 
 def get_app_favorites():
     # TODO: получение любимых вин
-    logging.info("Started getting app users")
+    logger.info("Started getting app users")
     app_users_id = []
     current_page = 0
     total_pages = 1
     while current_page < total_pages:
-        logging.info(f"Current page - {current_page} of {total_pages}")
+        logger.info(f"Current page - {current_page} of {total_pages}")
         app_users = requests.get(
             f"{USER_ADDRESS}/internal/users?page={current_page}&size=1"
         )
@@ -28,13 +30,13 @@ def get_app_favorites():
         total_pages = app_users_dict["totalPages"]
         current_page += 1
 
-    logging.info("Finished getting app users")
+    logger.info("Finished getting app users")
     return app_users_id
 
 
 def main():
     app_favorites = get_app_favorites()
-    logging.info(f"Started adding new favorites")
+    logger.info(f"Started adding new favorites")
     request_body = []
     for wine_id, user_id in tqdm(app_favorites):
         request_body.append(
@@ -42,13 +44,18 @@ def main():
         )
     response = requests.post(f"{OUR_ADDRESS}/review/", json=json.dumps(request_body))
     if response.status_code != 200:
-        logging.error(f"Adding favorites failed")
-        logging.error(response.status_code)
-        logging.error(response.text)
+        logger.error(f"Adding favorites failed")
+        logger.error(response.status_code)
+        logger.error(response.text)
         raise Exception("Adding favorites failed")
-    logging.info(f"Finished sync favorites")
+    logger.info(f"Finished sync favorites")
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger('kafka_reader')
+    logger.addHandler(logging.StreamHandler())
+    logger.addHandler(logstash.TCPLogstashHandler(host=logstash_host.split(":")[0],
+                                                  port=int(logstash_host.split(":")[1]),
+                                                  version=1))
     main()
