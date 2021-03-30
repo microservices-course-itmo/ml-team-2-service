d47e93a1156fde3c877f2c91b5ba69145fd32c3c
Bochkarev Evgeny <bochkarev_1906@mail.ru>
2021-03-30 20:45:23 +0700

    admin_edit

--- src/server/wineup/admin.py
+++ src/server/wineup/admin.py
@@ -2,6 +2,37 @@ from django.contrib import admin
 from .models import Wine, User, Review
 
 # Register your models here.
-admin.site.register(Wine)
-admin.site.register(User)
-admin.site.register(Review)
+
+
+class WineAdmin(admin.ModelAdmin):
+    fields = ('internal_id', 'all_names')
+    list_display = ('internal_id', 'all_names')
+    list_filter = ('internal_id', 'all_names')
+
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ('internal_id',)
+    list_display = ('internal_id',)
+    list_filter = ('internal_id',)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    fields = ('rating', 'variants', 'get_wine', 'get_user')
+    list_display = ('rating', 'variants', 'get_wine', 'get_user')
+
+    def get_wine(self, obj):
+        return obj.wine.internal_id
+    get_wine.admin_order_field = 'wine'
+    get_wine.short_description = 'Wine internal id'
+
+    def get_user(self, obj):
+        return obj.user.internal_id
+    get_user.admin_order_field = 'user'
+    get_user.short_description = 'User internal id'
+
+    list_filter = ('rating', 'variants', 'wine__internal_id', 'user__internal_id')
+
+
+admin.site.register(Wine, WineAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Review, ReviewAdmin)
